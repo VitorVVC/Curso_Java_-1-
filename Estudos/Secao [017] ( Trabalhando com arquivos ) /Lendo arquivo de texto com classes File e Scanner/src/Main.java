cf812cf0621740e858d6a102f8e1975f158f08db
Vitor <vitorvvc2005@gmail.com>
2024-01-23 13:26:45 -0300

    Iniciando estudos da secao [017]

---
+++ Estudos/Secao [017] ( Trabalhando com arquivos ) /Lendo arquivo de texto com classes File e Scanner/src/Main.java
@@ -0,0 +1,24 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        File file = new File("/Users/vitorvargas/Desktop/Cursos/Cursos JAVA/Curso Java --> 1/Estudos/Secao [017] ( Trabalhando com arquivos ) /Lendo arquivo de texto com classes File e Scanner/src/in.txt");
+        Scanner sc = null;
+
+        try {
+            sc = new Scanner(file);
+            while (sc.hasNextLine()) {
+                System.out.println(sc.nextLine());
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally { // Boa pratica: Fechar o nosso recurso em nosso finnally. Cuidando apenas se o scanner foi ou não instanciado para fugir de uma null pointer
+            if (sc != null) {
+                sc.close();
+            }
+        }
+    }
+}
