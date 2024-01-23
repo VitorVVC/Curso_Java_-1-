9af3cd3dcc2626ac24f994bdbd66fb31382e5f16
Vitor <vitorvvc2005@gmail.com>
2024-01-23 13:55:35 -0300

    Subindo aula de FileReader e BufferedReader, iniciando aula de Try-With-Resource

---
+++ Estudos/Secao [017] ( Trabalhando com arquivos ) /FileReader E BufferedReader/src/Main.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Main {
+    public static void main(String[] args) {
+        String path = "/Users/vitorvargas/Desktop/Cursos/Cursos JAVA/Curso Java --> 1/Estudos/Secao [017] ( Trabalhando com arquivos ) /FileReader E BufferedReader/src/in.txt";
+        // Testar a excecao:
+        //String path = "/Users/vitorvargas/Desktop/Cursos/Cursos JAVA/Curso Java --> 1/Estudos/Secao [017] ( Trabalhando com arquivos ) /FileReader E BufferedReader/src/ina.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            // Apartir da stream básica e apartir dela nos iniciamos o BufferedReader que tem leitura de buffers, oque torna mais flexiviel a leitura de arquivos.
+            br = new BufferedReader(fr);
+
+            String line = br.readLine();
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error: " + e.getMessage());
+        } finally {
+            try {
+                if (br != null) {
+                    br.close();
+                }
+                if (fr != null) {
+                    fr.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+        }
+
+    }
+}
